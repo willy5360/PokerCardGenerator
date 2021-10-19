10c7703c1e741105a0b532f1ab6043c75c7e412d
willy5360 <willy5360@hotmail.com>
2021-10-19 15:18:15 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,56 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const NUMBER_CENTER = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const ICON = ["♦", "♥", "♠", "♣"];
+const BODY = document.querySelector("body");
+const randomIcon = randomValue(ICON);
+const randomNumber = randomValue(NUMBER_CENTER);
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  createButton();
+  createCard();
 };
+
+function randomValue(list) {
+  return Math.floor(Math.random() * list.length);
+}
+
+function createButton() {
+  let button = document.createElement("button");
+  button.classList.add("btn-grad");
+  button.innerHTML = "Randomize me";
+  BODY.appendChild(button);
+  button.addEventListener("click", () => {
+    window.location.reload();
+  });
+}
+
+function createCard() {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  BODY.appendChild(card);
+
+  let top = document.createElement("div");
+  top.classList.add("card-topsuit");
+  top.innerHTML = ICON[randomIcon];
+  card.appendChild(top);
+
+  let center = document.createElement("div");
+  center.classList.add("card-centersuit");
+  center.innerHTML = NUMBER_CENTER[randomNumber];
+  card.appendChild(center);
+
+  let bottom = document.createElement("div");
+  bottom.classList.add("card-bottomsuit");
+  bottom.innerHTML = ICON[randomIcon];
+  card.appendChild(bottom);
+
+  if (ICON[randomIcon] == "♥" || ICON[randomIcon] == "♦") {
+    bottom.classList.add("color-red");
+    top.classList.add("color-red");
+  } else {
+    top.classList.add("color-black");
+    bottom.classList.add("color-black");
+  }
+}
